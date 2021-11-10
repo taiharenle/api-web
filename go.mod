ab1cdc77b6af31545d48fe460215ed1ab0e7c670
zx <16605100438@163.com>
2021-11-10 10:16:47 +0800

    加载静态资源

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/appleboy/gin-jwt/v2 v2.7.0
 	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/graphql-go/graphql v0.8.0
--- server/router.go
+++ server/router.go
@@ -4,6 +4,7 @@ import (
 	"api-web/api"
 	"api-web/middleware"
 	"api-web/model"
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,22 +20,30 @@ func NewRouter() *gin.Engine {
 	// 配置跨域
 	r.Use(middleware.Cors())
 
+	// 加载静态资源
+	r.Use(static.Serve("/", static.LocalFile("./build", true)))
+
 	// 路由
 	v := r.Group("/api")
 	{
-		v.GET("ping", api.Ping)
+		v.GET("/ping", api.Ping)
 
 		// 登录保护的路由
 		auth := v.Group("/v1")
 		auth.Use(authMiddleware.MiddlewareFunc())
 		{
 			// 挂载文件
-			auth.POST("upload", model.UploadFile)
-			auth.GET("download/:hash", model.DownloadFile)
+			auth.POST("/upload", model.UploadFile)
+			auth.GET("/download/:hash", model.DownloadFile)
 			// 挂载Graphql
 			auth.GET("/graphql", middleware.GraphqlHandler())
 			auth.POST("/graphql", middleware.GraphqlHandler())
 		}
 	}
+
+	r.NoRoute(func(c *gin.Context) {
+		c.File("./build/index.html")
+	})
+
 	return r
 }
